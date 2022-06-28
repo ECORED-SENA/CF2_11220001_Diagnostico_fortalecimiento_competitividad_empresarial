16ab2c9d363b3b62624844f10ea1d689c1209f30
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2022-06-28 18:57:12 -0500

    Correcciones 1
    Se actualizan videos tema 3.

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -8,40 +8,63 @@
       h1 Conclusiones diagnósticas y priorización estratégica competitiva      
     .row.justify-content-center.bg1
       .col-lg-10.mt-5
-      .col-lg-10.mt-5
         img(src='@/assets/curso/tema3/1.png', alt='Texto que describa la imagen')
       p.mt-5 Continuando con el proceso de diagnóstico, una vez sensibilizada la información y clasificada en oportunidades, amenazas, fortalezas y debilidades, como se trató previamente, es el momento de sacar las conclusiones diagnósticas del ejercicio realizado, identificando y estableciendo aquellos factores de mayor impacto para la empresa, bien sean favorables o desfavorables, lo que constituirá el marco referencial para la toma de decisiones, fijación de objetivos y demás aspectos del futuro inmediato de la empresa, en aras del desarrollo competitivo.
       p En el siguiente vídeo se observa cómo se pueden sintetizar las conclusiones del diagnóstico exógeno:
-    .tarjeta.tarjeta--azul1.p-4.mb-5
-      SlyderA(tipo="b")
+    .row.justify-content-center.bg72
+      .col-12.px-5
         .row.justify-content-center
+<<<<<<< Updated upstream
           .col-md-11.mb-4.mb-md-0
             .row.justify-content-center.mb-4
               .col-lg-8.col21
                 p.text-center.col23 <strong>Determinación de conclusiones diagnósticas exógenas</strong>        
             figure.mb-4
+=======
+          .col-lg-8
+            .titulo-quinto.color-acento-contenido
+              h4 Determinación de conclusiones diagnósticas exógenas
+          .col-lg-10.mb-5
+            figure
+>>>>>>> Stashed changes
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/ADrmedpQ_EU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-                
+                iframe(width="560" height="315" src="https://www.youtube.com/embed/phsEKaT_LJo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        p.mb-5 De igual manera para establecer las conclusiones de diagnóstico endógeno, y facilitar el proceso, se expone el siguiente video:
         .row.justify-content-center
+<<<<<<< Updated upstream
           .col-md-11.mb-4.mb-md-0
             .row.justify-content-center.mb-4
               p.text-center De igual manera para establecer las conclusiones de diagnóstico endógeno, y facilitar el proceso, se expone el siguiente video:
               .col-lg-8.col21
                 p.text-center.col23 <strong>Determinación de conclusiones diagnósticas endógenas</strong>     
             figure.mb-4
+=======
+          .col-lg-8
+            .titulo-quinto.color-acento-contenido
+              h4 Determinación de conclusiones diagnósticas endógenas
+          .col-lg-10.mb-5
+            figure
+>>>>>>> Stashed changes
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-           
+                iframe(width="560" height="315" src="https://www.youtube.com/embed/bEKXAX8hK_U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        p.mb-5 Las conclusiones diagnósticas generales para la competitividad se presentan en el siguiente video:
         .row.justify-content-center
+<<<<<<< Updated upstream
           .col-md-11.mb-4.mb-md-0
             .row.justify-content-center.mb-4
               p.text-center Las conclusiones diagnósticas generales para la competitividad se presentan en el siguiente video:
               .col-lg-8.col21
                 p.text-center.col23 <strong>Conclusiones diagnósticas generales</strong>  
             figure.mb-4
+=======
+          .col-lg-8
+            .titulo-quinto.color-acento-contenido
+              h4 Conclusiones diagnósticas generales
+          .col-lg-10
+            figure
+>>>>>>> Stashed changes
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                iframe(width="560" height="315" src="https://www.youtube.com/embed/52dohN9ODKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
          
 </template>
 
